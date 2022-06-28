f845717b4637925b7e8de880581c6cf708d69d31
Minatsu Iso <isoginchak0825@gmail.com>
2022-06-28 17:05:18 +0900

    feat: add output  course list

--- src/views/CourseListView.vue
+++ src/views/CourseListView.vue
@@ -1,5 +1,10 @@
 <template>
   <h2>course情報</h2>
+   <div v-for="course in axiosdata" :key="course.courseId">
+   <h2> {{ course.courseName }}</h2>
+   
+    <router-link :to="'/course/info/'+course.courseId">詳細を見る</router-link> 
+   </div>
 
 </template>
 
@@ -8,18 +13,26 @@ import {axios_domain} from "@/assets/axios_domain";
 import axios from "axios"
 export default {
   name: "CourseListView",
+  data() {
+    return {
+      axiosdata: {}
+    }
+   },
   mounted() {
     this.ajax_test()
   },
   methods:{
-    ajax_test:async function(){
+    ajax_test: async function () {
+    
 
-      const axiosURL = axios_domain+"/api/course/get_all"
-
-      const axiosResponse = await axios.get(axiosURL)
+     const axiosURL = axios_domain + "/api/course/get_all"
+     const axiosResponse = await axios.get(axiosURL)    
+      this.axiosdata = axiosResponse.data.courses;
+      console.log( axiosResponse )
     }
   }
 }
+
 </script>
 
 <style scoped>
