1da139fde990771630050276ee512cd83883a381
Minatsu Iso <isoginchak0825@gmail.com>
2022-06-28 15:34:38 +0900

    feat :add axios

--- src/views/CourseListView.vue
+++ src/views/CourseListView.vue
@@ -4,8 +4,21 @@
 </template>
 
 <script>
+import {axios_domain} from "@/assets/axios_domain";
+import axios from "axios"
 export default {
-  name: "CourseListView"
+  name: "CourseListView",
+  mounted() {
+    this.ajax_test()
+  },
+  methods:{
+    ajax_test:async function(){
+
+      const axiosURL = axios_domain+"/api/course/get_all"
+
+      const axiosResponse = await axios.get(axiosURL)
+    }
+  }
 }
 </script>
 
