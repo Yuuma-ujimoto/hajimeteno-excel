9c6a2cd08f686a64cb473b21a4c90d48e9025ddc
Yuuma-ujimoto <yxy.yuuma@gmail.com>
2022-07-15 09:57:15 +0900

    404エラーページ追加 フォント・CSS周りは後々修正予定

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
-const routes =[
-  {
-    name:"home",
-    component:import("@/views/CourseListView.vue"),
-    path:"/",
-    meta: { title: 'マンセル｜コース一覧' }
-  },
-  // {
-  //   name:"course list",
-  //   component:import("@/views/CourseListView.vue"),
-  //   path:"/course/list",
-  //   meta: { title: 'マンセル｜コース一覧' }
-  // },
-  {
-    name:"course info",
-    component:import("@/views/CourseInfoView.vue"),
-    path:"/course/info/:id",
-    meta: { title: 'マンセル｜コース詳細' }
+const routes = [
+    {
+        name: "home",
+        component: import("@/views/CourseListView.vue"),
+        path: "/",
+        meta: {title: 'マンセル｜コース一覧'}
+    },
+    // {
+    //   name:"course list",
+    //   component:import("@/views/CourseListView.vue"),
+    //   path:"/course/list",
+    //   meta: { title: 'マンセル｜コース一覧' }
+    // },
+    {
+        name: "course info",
+        component: import("@/views/CourseInfoView.vue"),
+        path: "/course/info/:id",
+        meta: {title: 'マンセル｜コース詳細'}
 
-  },
-  {
-    name:"chapter main",
-    path: "/exercise/:id",
-    component: import("@/views/ExerciseMainView.vue")
-  }
+    },
+    {
+        name: "chapter main",
+        path: "/exercise/:id",
+        component: import("@/views/ExerciseMainView.vue")
+    },
+
+    // 404エラー
+    // 上のルーティング全てに一致しなかった場合このページが呼ばれるので
+    // ページを追加することがある場合はここよりも上に追加してください
+    {
+        name: "404 error",
+        path: "/:catchAll(.*)",
+        component: import("@/views/404errorView.vue")
+    }
 ]
 
 const DEFAULT_TITLE = 'マンセル'
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 router.afterEach((to) => {
-  document.title = to.meta.title || DEFAULT_TITLE
+    document.title = to.meta.title || DEFAULT_TITLE
 })
 
 export default router
